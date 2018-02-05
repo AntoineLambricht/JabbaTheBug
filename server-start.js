79d5550c181665cfc708afffaa107217d94cbefd
Kyky <kyrilltircher14@hotmail.com>
2018-02-05 14:38:12 +0100

    mongo port

--- server-start.js
+++ server-start.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 process.env.SERVER_PORT = process.env.SERVER_PORT || '4040'
 process.env.MONGO_HOST = 'mongodb://ubuntu.tircher.be'
-process.env.MONGO_PORT = '27017'
+process.env.MONGO_PORT = '27020'
 require('babel-register');
 require("babel-polyfill");
 require('./server');
