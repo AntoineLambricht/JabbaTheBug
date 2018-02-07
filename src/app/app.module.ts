eb87c0234b7078c4ab731ce5f1345c46f9311770
Antoine Lambricht <lambricht@hotmail.com>
2018-02-07 20:38:51 +0100

    replace jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
 
 import { ConnectionService } from './services';
 
@@ -8,17 +9,26 @@ import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+//import { MachineComponent } from './machine/machine.component';
+import {FileSelectDirective} from "ng2-file-upload";
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnectionComponent,
-    HomeComponent
+    HomeComponent,
+    //MachineComponent,
+    FileSelectDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule
+    //FormBuilder,
+    //FormGroup,
+    //Validators
   ],
   providers: [
     ConnectionService
--- src/app/connection/connection.component.ts
+++ src/app/connection/connection.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConnectionService } from '../services';
-import * as $ from 'jquery';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import * as $ from 'jquery';
 
 //let $ = require('jquery');
 // declare var jquery:any;
@@ -16,8 +17,43 @@ import * as $ from 'jquery';
 export class ConnectionComponent implements OnInit {
 
   newLogin:boolean = true;
+  loginForm: FormGroup;
+  signinForm: FormGroup;
+  
+  
 
-  constructor(private _connectionService: ConnectionService) { }
+
+  constructor(private _connectionService: ConnectionService,private fb: FormBuilder) { 
+    this.createloginForm();
+    this.createSignInForm();
+  }
+
+
+  createloginForm() {
+    this.loginForm = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', [Validators.required,Validators.minLength(4)]]
+    });
+  }
+
+  createSignInForm(){
+    this.signinForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      mail: ['', [Validators.required,Validators.email]],
+      username: ['', Validators.required],
+      passwords: this.fb.group({
+        password: ['', [Validators.required,Validators.minLength(4)]],
+        passwordConf: ['', [Validators.required,Validators.minLength(4)]]
+      }, {validator: this.passwordConfirming})
+    });
+  }
+
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if(c.get('password').value !== c.get('passwordConf').value) {
+        return {invalid: true};
+    }
+}
 
   ngOnInit() {
   }
@@ -26,77 +62,17 @@ export class ConnectionComponent implements OnInit {
     this.newLogin = !this.newLogin;
   }
 
-  logIn(): void {
-    var form = $('#loginForm');
-    
-    var verif = verifChamps(form);
-
-    if(verif === "OK" && ($("#pwd_sign").val() !== $("#pwd_conf").val())){
-      verif = "Les password ne sont pas identiques";
-    }
-
-    if (verif === "OK") {
-      var json = getJsonValues(form);
-
-      //Call service
-      this._connectionService.login(json);
-
-    } else {
-      //Champs invalides
-      console.log("" + verif)
-      //this._alertService.error("" + verif);
-
-    }
-
-  }
-
-  signIn(): void {
-    var form = $('#signinForm');
-    var pass = $(form).find("input[name= 'password']").val();
-    var confpass = $(form).find("input[name= 'confirmed_password']").val();
-
-    var verif = verifChamps(form);
-
-    if (pass === confpass && verif === "OK") {
-
-      var json = getJsonValues(form);
-      //this._userService.signIn(json);
-    } else {
-      //Champs invalides
-      console.log("" + verif);
-      //this._alertService.warning("" + verif);
+  logIn(){
+    if(this.loginForm.valid){
+      this._connectionService.login(this.loginForm.value);
     }
   }
 
-}
-
-function getJsonValues(form: HTMLElement): object {
-  var values = {};
-
-  $(form).find('input').each(function () {
-    values[this.name] = $(this).val();
-  });
-
-  return values;
-}
-
-function verifChamps(form: HTMLElement): String {
-  var ret = "OK";
-  var pwd = "", pwdc = "";
-  
-  $(form).find('input').each(function () {
-
-    if (($(this).val() === "") && ret === "OK") {
-      ret = "You have to fill all the fields";
-    } else if ($(this).attr('name') === "mail" && ret === "OK") {
-      var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-      if (!pattern.test($(this).val())) {
-        ret = "Invalid mail";
-      }
-    } else if (ret === "OK" && ($(this).attr('name') === "password")&& $(this).val().length < 4) {
-        ret = "Password : 4 characters minimum";
+  signIn(){
+    if(this.signinForm.valid){
+      console.log(this.signinForm.value)
+      //this._userService.signIn(this.signinForm.value);
     }
-  });
+  }
 
-  return ret;
 }
