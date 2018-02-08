0652aa9a279f5eae5bc9220b16cb548d837325b8
Kyky <kyrilltircher14@hotmail.com>
2018-02-08 18:01:31 +0100

    debug mails

--- server/controllers/bug.controller.js
+++ server/controllers/bug.controller.js
@@ -17,14 +17,14 @@ function newBug(req, res, next) {
 			var bug = new Bug(req.body)
 			bug.save()
 				.then(savedBug => {
-					res.json(savedBug);
 					let structuredHtml = '<h1>JabbaTheBug was notified with a new bug</h1>';
 					structuredHtml += '<p>Machine :<b>' + savedBug.machinename + '</b></p>';
 					structuredHtml += '<hr><p>Mail of the sender :<b>' + savedBug.mailuser + '</b></p>';
 					structuredHtml += '<hr><p>Description of the bug :<b>' + savedBug.descrip + '</b></p>';
-					structuredHtml += '<hr><p>Photo : <img src="' + photo + '"/></p>';
+					//structuredHtml += '<hr><p>Photo : <img src="' + photo + '"/></p>';
 					//sends an email to all admins
 					nodemailer(structuredHtml);
+					res.json(savedBug);
 				})
 				.catch(e => next(e));
 		} else {
